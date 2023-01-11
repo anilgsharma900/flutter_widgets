c2ca3c3b4551112da8a9800a467edfe3726063a6
anilsharma <anils@winjit.com>
2023-01-11 22:00:01 +0530

    - Row And Column Widget With Their Properties

---
+++ lib/row_column_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class RowColumnWidget extends StatefulWidget {
+  const RowColumnWidget({Key? key}) : super(key: key);
+
+  @override
+  State<RowColumnWidget> createState() => _RowColumnWidgetState();
+}
+
+/// Column vertical
+// enter name
+// enter password
+//button
+
+///Row  horizontal
+// enter name     enter password       button
+
+class _RowColumnWidgetState extends State<RowColumnWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Row And Column"),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          // mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("First Name 1"),
+            const SizedBox(
+              height: 4.0,
+            ),
+            Text("First Name 2"),
+            SizedBox(
+              height: 4.0,
+            ),
+            Text("First Name 3"),
+            const SizedBox(
+              height: 4.0,
+            ),
+            Text("First Name 4"),
+            Text("First Name 5"),
+            const SizedBox(
+              height: 4.0,
+            ),
+            Text("First Name 6"),
+            Text("First Name 7"),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Container(
+              color: Colors.amber,
+              child: Row(
+                // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("First Name"),
+                  Text("Last Name"),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
