c45d9c5a63f9189e4bda900d21fa0a79b7ec51ec
anilsharma <anils@winjit.com>
2023-01-16 22:04:07 +0530

    WillPopScope: Handle backpressed

--- lib/row_column_widget.dart
+++ lib/row_column_widget.dart
@@ -16,55 +16,72 @@ class RowColumnWidget extends StatefulWidget {
 // enter name     enter password       button
 
 class _RowColumnWidgetState extends State<RowColumnWidget> {
+  bool doubleClickToExit = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Row And Column"),
-      ),
-      body: Container(
-        color: Colors.blue,
-        child: Column(
-          // mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("First Name 1"),
-            const SizedBox(
-              height: 4.0,
-            ),
-            Text("First Name 2"),
-            SizedBox(
-              height: 4.0,
-            ),
-            Text("First Name 3"),
-            const SizedBox(
-              height: 4.0,
-            ),
-            Text("First Name 4"),
-            Text("First Name 5"),
-            const SizedBox(
-              height: 4.0,
-            ),
-            Text("First Name 6"),
-            Text("First Name 7"),
-            const SizedBox(
-              height: 12.0,
-            ),
-            Container(
-              color: Colors.amber,
-              child: Row(
-                // mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text("First Name"),
-                  Text("Last Name"),
-                ],
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Row And Column"),
+        ),
+        body: Container(
+          color: Colors.blue,
+          child: Column(
+            // mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("First Name 1"),
+              const SizedBox(
+                height: 4.0,
               ),
-            )
-          ],
+              Text("First Name 2"),
+              SizedBox(
+                height: 4.0,
+              ),
+              Text("First Name 3"),
+              const SizedBox(
+                height: 4.0,
+              ),
+              Text("First Name 4"),
+              Text("First Name 5"),
+              const SizedBox(
+                height: 4.0,
+              ),
+              Text("First Name 6"),
+              Text("First Name 7"),
+              const SizedBox(
+                height: 12.0,
+              ),
+              Container(
+                color: Colors.amber,
+                child: Row(
+                  // mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("First Name"),
+                    Text("Last Name"),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
+      onWillPop: () async {
+        if (doubleClickToExit) {
+          return true;
+        }
+        doubleClickToExit = true;
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Double click to exit")));
+
+        Future.delayed(const Duration(seconds: 2), () {
+          doubleClickToExit = false;
+        });
+
+        return false;
+      },
     );
   }
 }
